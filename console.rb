d6d2cc8165fcb543c899d0dc903fdab84060bf19
Adamsh6 <adam.s.houten@gmail.com>
2019-05-21 10:17:11 +0100

    tested creating pizzaorder object

---
+++ console.rb
@@ -0,0 +1,22 @@
+require("pry")
+require_relative('models/pizza_order.rb')
+
+order1 = PizzaOrder.new(
+  {
+    'first_name' => 'Adam',
+    'last_name' => 'Houten',
+    'quantity' => 2,
+    'topping' => 'pepperoni'
+  }
+)
+
+order2 = PizzaOrder.new(
+  {
+    'first_name' => 'Louise',
+    'last_name' => 'Reed',
+    'quantity' => 1,
+    'topping' => 'cheese'
+  }
+)
+binding.pry
+nil
