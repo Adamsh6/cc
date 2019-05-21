6189c5a5729fd206704e7b7dc45bff6d29dbee16
Adamsh6 <adam.s.houten@gmail.com>
2019-05-21 13:26:14 +0100

    added id functionality to pizza_order class

--- models/pizza_order.rb
+++ models/pizza_order.rb
@@ -5,6 +5,7 @@ class PizzaOrder
   attr_accessor :first_name, :last_name, :quantity, :topping
 
   def initialize(options)
+    @id = options['id'].to_i if options['id']
     @first_name = options['first_name']
     @last_name = options['last_name']
     @quantity = options['quantity'].to_i
@@ -24,7 +25,8 @@ class PizzaOrder
             VALUES ($1, $2, $3, $4)"
     values = [@first_name, @last_name, @quantity, @topping]
     db.prepare("save", sql)
-    db.exec_prepared("save", values)
+    result = db.exec_prepared("save", values)
+    @id = result[0]['id'].to_i
     db.close
   end
 
