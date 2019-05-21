df9f372792708227d86076615f235fc350e550bb
Adamsh6 <adam.s.houten@gmail.com>
2019-05-21 09:57:19 +0100

    made pizza order class

---
+++ models/pizza_order.rb
@@ -0,0 +1,12 @@
+class PizzaOrder
+
+  attr_accessor :first_name, :last_name, :quantity, :topping
+
+  def initialize(options)
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @quantity = options['quantity'].to_i
+    @topping = options['topping']
+  end
+
+end
